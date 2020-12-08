ae07182720e8b0799ad92baceaa81cc5e5aafaec
Anders Hellerup Madsen <ahem@github.com>
2020-12-08 09:48:35 +0100

    day 8

---
+++ 8/main.ml
@@ -0,0 +1,49 @@
+open Core
+
+let input =
+  In_channel.input_lines In_channel.stdin
+  |> List.map ~f:(String.split ~on:' ')
+  |> List.map ~f:(function
+       | [ opcode; n ] -> (opcode, Int.of_string n)
+       | _ -> failwith "invalid input")
+  |> Array.of_list
+
+let rec walk ?(acc = 0) ?(pos = 0) ?(visited = Int.Set.empty) instructions =
+  if pos = Array.length instructions then `Finished acc
+  else if pos > Array.length instructions then `Invalid
+  else if Int.Set.mem visited pos then `Infinite_loop acc
+  else
+    let visited = Int.Set.add visited pos in
+    match instructions.(pos) with
+    | "nop", _ -> walk instructions ~pos:(pos + 1) ~acc ~visited
+    | "jmp", n -> walk instructions ~pos:(pos + n) ~acc ~visited
+    | "acc", n -> walk instructions ~pos:(pos + 1) ~acc:(acc + n) ~visited
+    | _ -> failwith "invalid instruction"
+
+let candidates : (string * int) array -> (string * int) array Sequence.t =
+ fun instructions ->
+  Sequence.range 0 (Array.length instructions)
+  |> Sequence.map ~f:(fun idx ->
+         let copy = Array.copy instructions in
+         match instructions.(idx) with
+         | "nop", n ->
+             copy.(idx) <- ("jmp", n);
+             Some copy
+         | "jmp", n ->
+             copy.(idx) <- ("nop", n);
+             Some copy
+         | _ -> None)
+  |> Sequence.filter_opt
+
+let () =
+  ( match walk input with
+  | `Infinite_loop n -> Printf.printf "part 1 result: %d\n" n
+  | _ -> failwith "no result for part 1!" );
+
+  let part2_result =
+    Sequence.find_map (candidates input) ~f:(fun candidate ->
+        match walk candidate with `Finished n -> Some n | _ -> None)
+  in
+  match part2_result with
+  | Some n -> Printf.printf "part 2 result: %d" n
+  | None -> failwith "no result for part 2!"
