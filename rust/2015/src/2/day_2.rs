f3dbfcb94210253d5debdcbc47bf93eee88d99bc
Anders Hellerup Madsen <ahem@github.com>
2021-05-06 21:29:13 +0200

    rust 2015, day 2

---
+++ rust/2015/src/2/day_2.rs
@@ -0,0 +1,40 @@
+use regex::Regex;
+use std::fs;
+
+fn main() {
+    let re = Regex::new(r"(\d+)x(\d+)x(\d+)").unwrap();
+    let input = fs::read_to_string("./src/2/input.txt").unwrap();
+    let values: Vec<(i32, i32, i32)> = re
+        .captures_iter(&input)
+        .map(|cap| {
+            (
+                cap[1].parse::<i32>().unwrap(),
+                cap[2].parse::<i32>().unwrap(),
+                cap[3].parse::<i32>().unwrap(),
+            )
+        })
+        .collect();
+
+    let part_1_result = values.iter().fold(0, |acc, (w, l, h)| {
+        let a = l * w;
+        let b = w * h;
+        let c = h * l;
+        return acc + 2 * a + 2 * b + 2 * c + a.min(b).min(c);
+    });
+
+    println!("part 1 result: {}", part_1_result);
+
+    let part_2_result = values.iter().fold(0, |acc, (w, l, h)| {
+        let a = w.min(l).min(h);
+        let b = if a == w {
+            l.min(h)
+        } else if a == l {
+            w.min(h)
+        } else {
+            w.min(l)
+        };
+        return acc + 2 * a + 2 * b + w * l * h;
+    });
+
+    println!("part 2 result: {}", part_2_result);
+}
