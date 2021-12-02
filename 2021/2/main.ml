4f8b2b1666933fa9abab3396edba6d6e2b63df54
Anders Hellerup Madsen <ahem@github.com>
2021-12-02 09:58:56 +0100

    2021 day2, ocaml

---
+++ 2021/2/main.ml
@@ -0,0 +1,33 @@
+let read_lines : in_channel -> string list = fun f ->
+    let rec loop = fun acc ->
+        try (loop ((input_line f) :: acc)) with
+        End_of_file -> acc
+    in
+    List.rev @@ loop []
+
+type op = Forward of int | Down of int | Up of int;;
+
+let parse_line s =
+    match (String.split_on_char ' ' s) with
+    | "forward" :: n :: [] -> Forward (int_of_string n)
+    | "down" :: n :: [] -> Down (int_of_string n)
+    | "up" :: n :: [] -> Up (int_of_string n)
+    | _ -> failwith "cannot parse line"
+
+let move (horizontal, depth) = function
+    | Forward n -> (horizontal + n, depth)
+    | Down n -> (horizontal, depth + n)
+    | Up n -> (horizontal, depth - n)
+
+let move_with_aim (horizontal, depth, aim) = function
+    | Forward n -> (horizontal + n, depth + aim * n, aim)
+    | Down n -> (horizontal, depth, aim + n)
+    | Up n -> (horizontal, depth, aim - n)
+
+
+let () =
+    let input = read_lines stdin |> List.map parse_line in
+    let horizontal, depth = List.fold_left move (0, 0) input in
+    Printf.printf "Part 1 final pos: %d, %d (%d)\n" horizontal depth (horizontal*depth);
+    let horizontal, depth, _ = List.fold_left move_with_aim (0, 0, 0) input in
+    Printf.printf "Part 2 final pos: %d, %d (%d)\n" horizontal depth (horizontal*depth)
