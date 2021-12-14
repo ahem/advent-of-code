971ad32672f3ed329fefdf17606bcadd858ed306
Anders Hellerup Madsen <ahem@github.com>
2021-12-14 10:16:35 +0100

    2021, day 14, part 2

--- 2021/14/main.ml
+++ 2021/14/main.ml
@@ -25,17 +25,16 @@ let polymer_of_string s =
   in
   loop PairMap.empty (List.of_seq @@ String.to_seq s)
 
-let step :
-    ((char * char) * (char * char)) PairMap.t -> int PairMap.t -> int PairMap.t
-    =
- fun pairs polymer ->
+let step pairs polymer =
   PairMap.fold
     (fun key n acc ->
       let pair_a, pair_b = PairMap.find key pairs in
       let acc = PairMap.update pair_a (incr n) acc in
       let acc = PairMap.update pair_b (incr n) acc in
       acc)
-    polymer polymer
+    polymer PairMap.empty
+
+let last_char s = s.[String.length s - 1]
 
 let count_chars : string -> int PairMap.t -> int CharMap.t =
  fun template polymer ->
@@ -47,31 +46,35 @@ let count_chars : string -> int PairMap.t -> int CharMap.t =
         acc)
       polymer CharMap.empty
   in
+  (* since pairs are overlapping, each letter is counted twice *)
+  let counts = CharMap.map (fun n -> n / 2) counts in
+  (* except the first and last in the template *)
   let counts = CharMap.update template.[0] (incr 1) counts in
-  let counts =
-    CharMap.update template.[String.length template - 1] (incr 1) counts
-  in
+  let counts = CharMap.update (last_char template) (incr 1) counts in
   counts
+
+let range n = Seq.unfold (fun i -> if i < n then Some (i, i + 1) else None) 0
+
+let score template polymer =
+  let quantities = count_chars template polymer in
+  let sorted =
+    quantities |> CharMap.bindings |> List.map snd |> List.sort_uniq Int.compare
+  in
+  let min = List.hd sorted in
+  let max = List.hd @@ List.rev sorted in
+  max - min
 
 let () =
   let template, pairs = read_input () in
-  let polymer = polymer_of_string template in
-  let polymer =
-    Seq.unfold (fun i -> if i < 10 then Some (i, i + 1) else None) 0
-    |> Seq.fold_left (fun acc _ -> step pairs acc) polymer
-  in
-  let quantities = count_chars template polymer in
-  CharMap.iter (Printf.printf "  %c: %n\n") quantities
 
-(*
-  B: 321208
-  C: 259212
-  F: 104292
-  H: 114194
-  K: 361654
-  N: 295284
-  O: 213592
-  P: 139008
-  S: 353823
-  V: 81597
-  *)
+  let polymer =
+    range 10
+    |> Seq.fold_left (fun acc _ -> step pairs acc) (polymer_of_string template)
+  in
+  Printf.printf "Part 1: %d\n" @@ score template polymer;
+
+  let polymer =
+    range 40
+    |> Seq.fold_left (fun acc _ -> step pairs acc) (polymer_of_string template)
+  in
+  Printf.printf "Part 2: %d\n" @@ score template polymer
