e174ea064b305dec987f535e13ab1b0d1c598c70
Anders Hellerup Madsen <ahem@github.com>
2022-12-01 15:04:32 +0100

    2022, day 1, rust - cleanup

--- rust/2022/src/1/day_1.rs
+++ rust/2022/src/1/day_1.rs
@@ -1,46 +1,18 @@
 use std::{io, io::prelude::*};
 
 fn main() {
-    /*
-    use itertools::Itertools;
-    let lst = io::stdin()
-        .lock()
-        .lines()
-        .map(|l| l.unwrap().parse::<u32>())
-        .group_by(|x| x.is_ok())
-        .into_iter()
-        .filter_map(|x| {
-            if x.0 {
-                Some(x.1.into_iter().map(|x| x.unwrap()).sum::<u32>())
-            } else {
-                None
-            }
-        })
-        .collect_vec();
-
-    let part_1_result = lst.iter().max().unwrap();
-    println!("part 1: {part_1_result}");
-
-    let part_2_result = lst.iter().sorted().rev().take(3).sum::<u32>();
-    println!("part 2: {part_2_result}");
-    */
-
-    let mut lst = vec![];
-    let mut sum = 0;
-    for line in io::stdin().lock().lines() {
+    let mut lst = io::stdin().lock().lines().fold(vec![0], |mut acc, line| {
         match line.unwrap().parse::<u32>() {
-            Ok(n) => sum += n,
-            Err(_) => {
-                lst.push(sum);
-                sum = 0;
-            }
+            Ok(n) => *acc.last_mut().unwrap() += n,
+            Err(_) => acc.push(0),
         }
-    }
+        acc
+    });
+    lst.sort();
 
-    let part_1_result = lst.iter().max().unwrap();
+    let part_1_result = lst.last().unwrap();
     println!("part 1: {part_1_result}");
 
-    lst.sort();
     let part_2_result = lst.iter().rev().take(3).sum::<u32>();
     println!("part 2: {part_2_result}");
 }
