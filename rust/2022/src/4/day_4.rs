7c83515a543598fb637d3965beba392660390d1e
Anders Hellerup Madsen <ahem@github.com>
2022-12-04 10:58:46 +0100

    2022, day 4, rust - cleanup

--- rust/2022/src/4/day_4.rs
+++ rust/2022/src/4/day_4.rs
@@ -1,41 +1,68 @@
-type TRange = std::ops::RangeInclusive<u32>;
-type TPair = (TRange, TRange);
+mod assignment {
+    pub struct Assignment {
+        range: std::ops::RangeInclusive<u32>,
+    }
 
-fn parse_line(s: &str) -> Option<TPair> {
-    let mut iter = s.splitn(2, ",");
-    Some((parse_pair(iter.next()?)?, parse_pair(iter.next()?)?))
+    #[derive(Debug)]
+    pub struct ParseError;
+
+    impl Assignment {
+        pub fn contains(&self, other: &Self) -> bool {
+            self.range.clone().all(|c| other.range.contains(&c))
+        }
+
+        pub fn overlaps(&self, other: &Self) -> bool {
+            self.range.clone().any(|c| other.range.contains(&c))
+        }
+    }
+
+    impl std::str::FromStr for Assignment {
+        type Err = ParseError;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let mut iter = s
+                .splitn(2, "-")
+                .map(|s| s.parse::<u32>().or(Err(ParseError)));
+            let a = iter.next().ok_or(ParseError)??;
+            let b = iter.next().ok_or(ParseError)??;
+            Ok(Assignment { range: a..=b })
+        }
+    }
+
+    pub type AssignmentPair = (Assignment, Assignment);
+
+    pub fn parse_pair(s: &str) -> Result<AssignmentPair, ParseError> {
+        let mut iter = s.splitn(2, ",").map(|s| s.parse::<Assignment>());
+        let a = iter.next().ok_or(ParseError)??;
+        let b = iter.next().ok_or(ParseError)??;
+        Ok((a, b))
+    }
 }
 
-fn parse_pair(s: &str) -> Option<TRange> {
-    let mut iter = s.splitn(2, "-");
-    let a = iter.next()?.parse::<u32>().ok()?;
-    let b = iter.next()?.parse::<u32>().ok()?;
-    Some(a..=b)
+use assignment::AssignmentPair;
+
+#[derive(Debug)]
+enum Error {
+    ParseError(assignment::ParseError),
+    IOError(std::io::Error),
 }
 
-fn read_input() -> Vec<TPair> {
+fn read_input() -> Result<Vec<AssignmentPair>, Error> {
     std::io::stdin()
         .lines()
-        .map(|s| parse_line(s.unwrap().as_str()).unwrap())
+        .map(|s| s.map_err(Error::IOError))
+        .map(|s| assignment::parse_pair(s?.as_str()).map_err(Error::ParseError))
         .collect()
 }
 
-fn contains(a: &TRange, b: &TRange) -> bool {
-    b.clone().all(|c| a.contains(&c))
-}
-
-fn overlaps(a: &TRange, b: &TRange) -> bool {
-    b.clone().any(|c| a.contains(&c))
-}
-
 pub fn main() {
-    let input = read_input();
-    let part_1_result = input
+    let pairs = read_input().unwrap();
+    let part_1_result = pairs
         .iter()
-        .filter(|(a, b)| contains(a, b) || contains(b, a))
+        .filter(|(a, b)| a.contains(b) || b.contains(a))
         .count();
     println!("part 1: {part_1_result}");
 
-    let part_2_result = input.iter().filter(|(a, b)| overlaps(a, b)).count();
+    let part_2_result = pairs.iter().filter(|(a, b)| a.overlaps(b)).count();
     println!("part 2: {part_2_result}");
 }
